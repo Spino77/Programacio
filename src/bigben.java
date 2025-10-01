d595f62783533471840f5009bb63bd507369f281
pau <duquemartinezpau@elpuig.xeill.net>
2025-10-01 17:33:18 +0200

    paella

--- src/bigben.java
+++ src/bigben.java
@@ -1,2 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class bigben {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int horaactual = sc.nextInt();
+        int suma = sc.nextInt();
+
+        int modul = suma + horaactual;
+        int horafinal = modul % 12;
+
+        System.out.println(horafinal);
+
+    }
 }
