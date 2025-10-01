58950e57b8865927dccbf8f69819e7c03d6f1e6a
pau <duquemartinezpau@elpuig.xeill.net>
2025-10-01 17:04:45 +0200

    paella

--- src/paella.java
+++ src/paella.java
@@ -6,7 +6,25 @@ public class paella {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        float comensals = sc.nextFloat();
+        float preuarrosperkilo = sc.nextFloat();
+        float preugambesperkilo = sc.nextFloat();
 
+        float comensals4 = comensals / 4;
+
+        float pesarros = (float) ((float) comensals4 * 0.5);
+        float pesgambes = (float) ((float) comensals4 * 0.25);
+
+        float preuarros = preuarrosperkilo * pesarros;
+        float preugambes = preugambesperkilo * pesgambes;
+
+        float total = preuarros + preugambes;
+
+        System.out.println(pesarros + " kg arros");
+        System.out.println(pesgambes + " kg gambes");
+        System.out.println(preuarros + " euros arros");
+        System.out.println(preugambes + " euros gambes");
+        System.out.println("TOTAL: " + total + " euros");
 
     }
 }
