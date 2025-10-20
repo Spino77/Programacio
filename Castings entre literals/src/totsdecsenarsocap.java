62af8bf8dd94e07c7b722255bb2dd8b653d35313
pau <duquemartinezpau@elpuig.xeill.net>
2025-10-20 16:38:37 +0200

    ayuda

--- Castings entre literals/src/totsdecsenarsocap.java
+++ Castings entre literals/src/totsdecsenarsocap.java
@@ -6,9 +6,21 @@ public class totsdecsenarsocap {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        float num1 = sc.nextFloat();
+        float num2 = sc.nextFloat();
+        float num3 = sc.nextFloat();
 
+        int num1_int = (int)num1;
+        int num2_int = (int)num2;
+        int num3_int = (int)num3;
 
+        boolean parell1 = (num1_int % 2) > 0;
+        boolean parell2 = (num2_int % 2) > 0;
+        boolean parell3 = (num3_int % 2) > 0;
 
+        boolean ayuda = (parell1 && parell2 && parell3) || (!parell1 && !parell2 && !parell3);
+
+        System.out.println(ayuda);
 
     }
 }
