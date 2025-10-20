4ccb5acc9b35ea942de272c771b2cb67d6530bb3
Спино <duquemartinezpau@elpuig.xeill.net>
2025-10-20 13:06:11 +0200

    Update c1l22provamecano.java
    ayuda

--- Castings entre literals/src/c1l22provamecano.java
+++ Castings entre literals/src/c1l22provamecano.java
@@ -6,18 +6,18 @@ public class c1l22provamecano {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
-        float C = sc.nextInt();
-        float E = sc.nextInt();
-        float T = sc.nextInt();
+        float C = sc.nextFloat();
+        float E = sc.nextFloat();
+        float T = sc.nextFloat();
 
-        float correctes = C - E;
-        int percentatge = (int) ((correctes / C) * 100);
-        float minuts = (float) T / 60;
-        float velocitat_float = (C / minuts) / 5;
-        int velocitat = (int) velocitat_float;
+        float percentatge_float = ((C - E) / C) * 100;
+        int percentatge = (int)(percentatge_float); 
+
+        float minuts = T / 60f;
+        float velocitat_float = (C / 5f) / minuts;
+        int velocitat = (int)(velocitat_float); 
 
         System.out.println(percentatge);
         System.out.println(velocitat);
-
     }
 }
