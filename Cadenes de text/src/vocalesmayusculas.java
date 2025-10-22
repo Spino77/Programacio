dcd4a191aa1fd6bd0f92af9a013c69ce65480885
pau <duquemartinezpau@elpuig.xeill.net>
2025-10-22 17:50:06 +0200

    ayuda

---
+++ Cadenes de text/src/vocalesmayusculas.java
@@ -0,0 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class vocalesmayusculas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        String S = "";
+
+
+
+
+    }
+}
