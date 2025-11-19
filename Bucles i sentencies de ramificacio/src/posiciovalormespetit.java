ed2c5903e12ca01c9ca696b9bb4b25469b1d7c83
pau <duquemartinezpau@elpuig.xeill.net>
2025-11-19 17:37:38 +0100

    ayuda

---
+++ Bucles i sentencies de ramificacio/src/posiciovalormespetit.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class posiciovalormespetit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+        int Nt = sc.nextInt();
+        int Ni = sc.nextInt();
+        int P = 1;
+        int Pf = 0;
+        int Np = Ni;
+
+        for (int i = 0; i < Nt; i++) {
+            if (Ni <= Np) {
+                Np = Ni;
+                Pf = P;
+            }
+
+            P += 1;
+            Ni = sc.nextInt();
+        }
+
+        System.out.println("El nombre més petit és el " + Np + " i es troba a la posició " + Pf);
+    }
+}
