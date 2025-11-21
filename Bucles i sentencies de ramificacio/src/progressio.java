1fac9ecc0d52802d96c0c221943a0e7bb6f2614f
pau <duquemartinezpau@elpuig.xeill.net>
2025-11-21 17:33:50 +0100

    ayuda

--- Bucles i sentencies de ramificacio/src/progressio.java
+++ Bucles i sentencies de ramificacio/src/progressio.java
@@ -6,7 +6,37 @@ public class progressio {
         Scanner sc = new Scanner(System.in);
         sc.useLocale(Locale.ENGLISH);
 
+        int N = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean hola = false;
 
+        int progresio = b - a;
+
+        while (N != 1) {
+            if (a + progresio == b) {
+                hola = true;
+            } else {
+                hola = false;
+                break;
+            }
+
+            N -= 1;
+
+            if (N == 1) {
+                break;
+            }
+
+            a = b;
+            b = sc.nextInt();
+
+        }
+
+        if (hola == true) {
+            System.out.println("si");
+        } else if (hola == false) {
+            System.out.println("no");
+        }
 
 
     }
