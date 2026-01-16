b3bc40475c9a7de4c6f102b0847b829e95d4df2e
pauduquemartinez <duquemartinezpau@elpuig.xeill.net>
2026-01-16 17:53:20 +0100

    metodes facil o no

---
+++ Metodes/src/c2l03calculadora.java
@@ -0,0 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class c2l03calculadora {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.ENGLISH);
+
+
+
+
+
+    }
+}
